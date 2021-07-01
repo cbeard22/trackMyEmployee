3490865b54ed85bf74da88d2761e9220efedeea4
Chris Beard <cbeard@hnoj.org>
2021-07-01 14:57:16 -0500

    initial commit adding schema.sql and switch and connection to schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+	 id INT NOT NULL AUTO_INCREMENT,
+     NAME VARCHAR(30) NOT NULL,
+     PRIMARY KEY (id)
+     );
+
+CREATE TABLE role (
+	 id INT NOT NULL AUTO_INCREMENT,
+     title VARCHAR(30) NOT NULL,
+     salary DECIMAL(10,2) NOT NULL,
+     department_id INT NOT NULL,
+     PRIMARY KEY(id),
+     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+     );
+     
+CREATE TABLE employee (
+ id INT NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(30) NOT NULL,
+ last_name VARCHAR(30) NOT NULL,
+ role_id INT NOT NULL,
+ manager_id INT,
+ FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+ FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE,
+ PRIMARY KEY(id)
+ );
